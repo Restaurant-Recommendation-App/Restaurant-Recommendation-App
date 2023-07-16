1ef3c2723380e803293c25a0888722f91e714b0d
sseoju <wngus606@gmail.com>
2023-07-16 23:08:32 +0900

    add image assets

--- Cheffi/Application/AppFlowCoordinator.swift
+++ Cheffi/Application/AppFlowCoordinator.swift
@@ -6,7 +6,6 @@
 //
 
 import UIKit
-import SwifterSwift
 
 
 /// 앱의 화면 전환을 담당하는 코디네이터
@@ -21,11 +20,17 @@ final class AppFlowCoordinator {
     
     func start() {
         tabBarController.tabBar.isTranslucent = false
+        addLineView()
+        
+        let normalAttributeName: [NSAttributedString.Key : Any] = [NSAttributedString.Key.foregroundColor: UIColor(hexString: "B1B1B1")!, NSAttributedString.Key.font: UIFont.systemFont(ofSize: 12, weight: .regular)]
+        let selectedAttributeName: [NSAttributedString.Key : Any] = [NSAttributedString.Key.foregroundColor: UIColor(hexString: "D82231")!, NSAttributedString.Key.font: UIFont.systemFont(ofSize: 12, weight: .regular)]
         
         let homeSceneDIContainer = appDIContainer.makeHomeSceneDIContainer()
         let homeTabBarItem = UITabBarItem(title: "홈".localized(),
-                                          image: UIImage(named: "")?.withRenderingMode(.alwaysOriginal),
-                                          selectedImage: UIImage(named: "")?.withRenderingMode(.alwaysOriginal))
+                                          image: UIImage(named: "icHomeSelected")?.withRenderingMode(.alwaysOriginal),
+                                          selectedImage: UIImage(named: "icHomeSelected")?.withRenderingMode(.alwaysOriginal))
+        homeTabBarItem.setTitleTextAttributes(normalAttributeName, for: .normal)
+        homeTabBarItem.setTitleTextAttributes(selectedAttributeName, for: .selected)
         let homeNavigationController = createNavigationController(with: homeTabBarItem)
         let homeViewModel = homeSceneDIContainer.makeHomeViewModel()
         let homeCoordinator = homeSceneDIContainer.makeHomeFlowCoordinator(
@@ -36,8 +41,10 @@ final class AppFlowCoordinator {
         
         let nationalTrendSceneDIContainer = appDIContainer.makeNationalTrendSceneDIContainer()
         let nationalTrendTabBarItem = UITabBarItem(title: "전국트랜드".localized(),
-                                                   image: UIImage(named: "")?.withRenderingMode(.alwaysOriginal),
-                                                   selectedImage: UIImage(named: "")?.withRenderingMode(.alwaysOriginal))
+                                                   image: UIImage(named: "icNationalTrendDefault")?.withRenderingMode(.alwaysOriginal),
+                                                   selectedImage: UIImage(named: "icNationalTrendDefault")?.withRenderingMode(.alwaysOriginal))
+        nationalTrendTabBarItem.setTitleTextAttributes(normalAttributeName, for: .normal)
+        nationalTrendTabBarItem.setTitleTextAttributes(selectedAttributeName, for: .selected)
         let nationalTrendNavigationController = createNavigationController(with: nationalTrendTabBarItem)
         let nationalTrendCoordinator = nationalTrendSceneDIContainer.makeNationalTrendFlowCoordinator(
             navigationController: nationalTrendNavigationController,
@@ -47,8 +54,10 @@ final class AppFlowCoordinator {
         
         let restaurantRegistSceneDIContainer = appDIContainer.makeRestaurantRegistSceneDIContainer()
         let restaurantRegistTabBarItem = UITabBarItem(title: "맛집등록".localized(),
-                                                      image: UIImage(named: "")?.withRenderingMode(.alwaysOriginal),
-                                                      selectedImage: UIImage(named: "")?.withRenderingMode(.alwaysOriginal))
+                                                      image: UIImage(named: "icRestaurantRegistDefault")?.withRenderingMode(.alwaysOriginal),
+                                                      selectedImage: UIImage(named: "icRestaurantRegistDefault")?.withRenderingMode(.alwaysOriginal))
+        restaurantRegistTabBarItem.setTitleTextAttributes(normalAttributeName, for: .normal)
+        restaurantRegistTabBarItem.setTitleTextAttributes(selectedAttributeName, for: .selected)
         let restaurantRegistNavigationController = createNavigationController(with: restaurantRegistTabBarItem)
         let restaurantRegistCoordinator = restaurantRegistSceneDIContainer.makeRestaurantRegistFlowCoordinator(
             navigationController: restaurantRegistNavigationController,
@@ -58,8 +67,10 @@ final class AppFlowCoordinator {
         
         let myPageSceneDIContainer = appDIContainer.makeMyPageFlowCoordinator()
         let myPageTabBarItem = UITabBarItem(title: "마이페이지".localized(),
-                                            image: UIImage(named: "")?.withRenderingMode(.alwaysOriginal),
-                                            selectedImage: UIImage(named: "")?.withRenderingMode(.alwaysOriginal))
+                                            image: UIImage(named: "icMyPageDefault")?.withRenderingMode(.alwaysOriginal),
+                                            selectedImage: UIImage(named: "icMyPageDefault")?.withRenderingMode(.alwaysOriginal))
+        myPageTabBarItem.setTitleTextAttributes(normalAttributeName, for: .normal)
+        myPageTabBarItem.setTitleTextAttributes(selectedAttributeName, for: .selected)
         let myPageNavigationController = createNavigationController(with: myPageTabBarItem)
         let myPageCoordinator = myPageSceneDIContainer.makeMyPageFlowCoordinator(
             navigationController: myPageNavigationController,
@@ -82,5 +93,17 @@ final class AppFlowCoordinator {
         navigationController.navigationBar.isTranslucent = false
         navigationController.setNavigationBarHidden(true, animated: false)
         return navigationController
+    }
+    
+    private func addLineView() {
+        let lineView: UIView = UIView()
+        lineView.backgroundColor = UIColor(hexString: "ECECEC")
+        tabBarController.tabBar.addSubview(lineView)
+
+        lineView.snp.makeConstraints { make in
+            make.height.equalTo(1)
+            make.top.equalToSuperview()
+            make.left.right.equalToSuperview()
+        }
     }
 }
