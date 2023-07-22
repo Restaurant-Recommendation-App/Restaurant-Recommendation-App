d3d9215209b611f519238ae7a08638fb14cb0471
sseoju <wngus606@gmail.com>
2023-07-22 14:36:55 +0900

    add color extension

---
+++ Cheffi/Common/Extensions/UIColor+Extensions.swift
@@ -0,0 +1,24 @@
+//
+//  UIColor+Extensions.swift
+//  Cheffi
+//
+//  Created by USER on 2023/07/22.
+//
+
+import UIKit
+
+extension UIColor {
+    static let main: UIColor = UIColor(named: "Main")!
+    static let cheffiBlack: UIColor = UIColor(named: "CheffiBlack")!
+    static let cheffiWhite: UIColor = UIColor(named: "CheffiWhite")!
+    static let cheffiWhite05: UIColor = UIColor.cheffiBlack.withAlphaComponent(0.05)
+    static let cheffiGray1: UIColor = UIColor.cheffiBlack.withAlphaComponent(0.1)
+    static let cheffiGray2: UIColor = UIColor.cheffiBlack.withAlphaComponent(0.2)
+    static let cheffiGray3: UIColor = UIColor.cheffiBlack.withAlphaComponent(0.3)
+    static let cheffiGray4: UIColor = UIColor.cheffiBlack.withAlphaComponent(0.4)
+    static let cheffiGray5: UIColor = UIColor.cheffiBlack.withAlphaComponent(0.5)
+    static let cheffiGray6: UIColor = UIColor.cheffiBlack.withAlphaComponent(0.6)
+    static let cheffiGray7: UIColor = UIColor.cheffiBlack.withAlphaComponent(0.7)
+    static let cheffiGray8: UIColor = UIColor.cheffiBlack.withAlphaComponent(0.8)
+    static let cheffiGray9: UIColor = UIColor.cheffiBlack.withAlphaComponent(0.9)
+}
