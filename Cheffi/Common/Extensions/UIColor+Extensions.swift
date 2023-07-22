5eab64e020afeb06c7dc29d8f2bb66b08ef3ec08
sseoju <wngus606@gmail.com>
2023-07-22 15:14:17 +0900

    update storyboard for SimilarChefCell

--- Cheffi/Common/Extensions/UIColor+Extensions.swift
+++ Cheffi/Common/Extensions/UIColor+Extensions.swift
@@ -11,14 +11,14 @@ extension UIColor {
     static let main: UIColor = UIColor(named: "Main")!
     static let cheffiBlack: UIColor = UIColor(named: "CheffiBlack")!
     static let cheffiWhite: UIColor = UIColor(named: "CheffiWhite")!
-    static let cheffiWhite05: UIColor = UIColor.cheffiBlack.withAlphaComponent(0.05)
-    static let cheffiGray1: UIColor = UIColor.cheffiBlack.withAlphaComponent(0.1)
-    static let cheffiGray2: UIColor = UIColor.cheffiBlack.withAlphaComponent(0.2)
-    static let cheffiGray3: UIColor = UIColor.cheffiBlack.withAlphaComponent(0.3)
-    static let cheffiGray4: UIColor = UIColor.cheffiBlack.withAlphaComponent(0.4)
-    static let cheffiGray5: UIColor = UIColor.cheffiBlack.withAlphaComponent(0.5)
-    static let cheffiGray6: UIColor = UIColor.cheffiBlack.withAlphaComponent(0.6)
-    static let cheffiGray7: UIColor = UIColor.cheffiBlack.withAlphaComponent(0.7)
-    static let cheffiGray8: UIColor = UIColor.cheffiBlack.withAlphaComponent(0.8)
-    static let cheffiGray9: UIColor = UIColor.cheffiBlack.withAlphaComponent(0.9)
+    static let cheffiWhite05: UIColor = UIColor(named: "CheffiWhite05")!
+    static let cheffiGray1: UIColor = UIColor(named: "CheffiGray1")!
+    static let cheffiGray2: UIColor = UIColor(named: "CheffiGray2")!
+    static let cheffiGray3: UIColor = UIColor(named: "CheffiGray3")!
+    static let cheffiGray4: UIColor = UIColor(named: "CheffiGray4")!
+    static let cheffiGray5: UIColor = UIColor(named: "CheffiGray5")!
+    static let cheffiGray6: UIColor = UIColor(named: "CheffiGray6")!
+    static let cheffiGray7: UIColor = UIColor(named: "CheffiGray7")!
+    static let cheffiGray8: UIColor = UIColor(named: "CheffiGray8")!
+    static let cheffiGray9: UIColor = UIColor(named: "CheffiGray9")!
 }
