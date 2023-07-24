4e2901bf003cb6b74c0d90fcf7e963a108e0f9a7
ronick-grammer <glflakcm@gmail.com>
2023-07-24 16:45:47 +0900

    wip: :recycle: 홈화면 인기급등맛집 섹션 타이틀 아이콘 추가 작업

--- Cheffi/Presentation/HomeScene/Views/PopularRestaurant/View/MainPopularRestaurantView.swift
+++ Cheffi/Presentation/HomeScene/Views/PopularRestaurant/View/MainPopularRestaurantView.swift
@@ -31,6 +31,15 @@ class MainPopularRestaurantView: UIView {
         return label
     }()
     
+    private lazy var exclamationButton: UIButton = {
+        let button = UIButton()
+        let image = UIImage(named: "icExclamationMark.circle")
+        button.setImage(image, for: .normal)
+        button.imageView?.contentMode = .scaleAspectFit
+        button.addTarget(self, action: #selector(tappedExclamation), for: .touchUpInside)
+        return button
+    }()
+    
     private let restaurantContentView = RestaurantContentView()
     
     override init(frame: CGRect) {
@@ -53,7 +62,13 @@ class MainPopularRestaurantView: UIView {
         addSubview(subtitleLabel)
         subtitleLabel.snp.makeConstraints {
             $0.top.equalTo(titleLabel.snp.bottom).offset(16)
-            $0.leading.trailing.equalToSuperview()
+            $0.leading.equalToSuperview()
+        }
+        
+        addSubview(exclamationButton)
+        exclamationButton.snp.makeConstraints {
+            $0.leading.equalTo(subtitleLabel.snp.trailing).offset(4)
+            $0.bottom.equalTo(subtitleLabel)
         }
         
         addSubview(restaurantContentView)
@@ -64,14 +79,21 @@ class MainPopularRestaurantView: UIView {
     }
     
     private func getSubtitleAttributedString() -> NSAttributedString {
-        let str1 = "00 : 13 : 43"
+        
+        let icClockAttachment = NSTextAttachment()
+        let icClockImg = UIImage(named: "icClock")
+        icClockAttachment.image = icClockImg
+        icClockAttachment.bounds = CGRect(x: 0, y: -2, width: icClockImg!.size.width, height: icClockImg!.size.height)
+        let icClockString = NSAttributedString(attachment: icClockAttachment)
+        
+        let str1 = "  00 : 13 : 43"
         let color1 = UIColor(hexString: "D82231")
         let font1 = UIFont.systemFont(ofSize: 18, weight: .bold)
         
         let str2 = " 초 뒤에\n인기 급등 맛집이 변경돼요."
         let color2 = UIColor.black
         let font2 = UIFont.systemFont(ofSize: 18, weight: .light)
-
+        
         let combination = NSMutableAttributedString()
         
         let attr1 = [NSAttributedString.Key.foregroundColor: color1, NSAttributedString.Key.font: font1]
@@ -80,10 +102,15 @@ class MainPopularRestaurantView: UIView {
         let attr2 = [NSAttributedString.Key.foregroundColor: color2, NSAttributedString.Key.font: font2]
         let part2 = NSMutableAttributedString(string: str2, attributes: attr2)
         
+        combination.append(icClockString)
         combination.append(part1)
         combination.append(part2)
         
         return combination
+    }
+    
+    @objc private func tappedExclamation() {
+        print("tappedExclamation")
     }
 }
 
