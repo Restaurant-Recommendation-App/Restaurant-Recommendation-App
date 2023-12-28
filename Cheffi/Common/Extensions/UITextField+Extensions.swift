b589b66b9afbe0740140a335fa5fd6ea1fac7607
ronick-grammer <glflakcm@gmail.com>
2023-12-29 01:29:57 +0900

    [40] textField 마진 조정된 left icon 추가 메서드 작업

--- Cheffi/Common/Extensions/UITextField+Extensions.swift
+++ Cheffi/Common/Extensions/UITextField+Extensions.swift
@@ -16,4 +16,21 @@ extension UITextField {
             }
             .eraseToAnyPublisher()
     }
+    
+    func addLeftIcon(withImage image: UIImage?, imageSize: CGSize) {
+        let leftPadding: CGFloat = 16
+        let rightPadding: CGFloat = 8
+        
+        let leftView = UIView(frame: CGRect(x: 0, y: 0, width: leftPadding + imageSize.width + rightPadding, height: frame.height))
+        let centerY = leftView.frame.midY - (imageSize.height / 2)
+        let imageView = UIImageView(frame: CGRect(x: leftPadding, y: centerY, width: imageSize.width, height: imageSize.height))
+        
+        imageView.image = image
+        imageView.contentMode = .scaleAspectFit
+        
+        leftView.addSubview(imageView)
+        
+        self.leftView = leftView
+        self.leftViewMode = .always
+    }
 }
