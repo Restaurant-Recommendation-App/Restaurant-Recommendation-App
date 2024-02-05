a7d137002c6e4b5ba46766edd069cb63ca441121
Yeonghyeon Kim <ronick@bubbletap.com>
2024-02-05 23:19:39 +0900

    [64] Address 에서 사용되지 않는 데이터 제거

--- Cheffi/Domain/Entities/Address.swift
+++ Cheffi/Domain/Entities/Address.swift
@@ -11,7 +11,6 @@ import Foundation
 struct Address: Codable, Hashable {
     let province: String // 식당의 시/도 주소(1차)
     let city: String // 식당의 시/군/구 주소(2차)
-    let lotNumber: String // 지번 주소
     let roadName: String // 도로명 주소
     let fullLotNumberAddress: String // 식당 전체 지번 주소
     let fullRodNameAddress: String // 식당 전체 도로명 주소
