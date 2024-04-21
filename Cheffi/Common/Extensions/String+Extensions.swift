e71bc339f0ce32a5a9209b363d391abbea8d2891
Yeonghyeon Kim <ronick@bubbletap.com>
2024-04-21 17:36:55 +0900

    #98 닉네임 유효성 검사 추가

--- Cheffi/Common/Extensions/String+Extensions.swift
+++ Cheffi/Common/Extensions/String+Extensions.swift
@@ -16,3 +16,17 @@ extension String {
         self = self.capitalizingFirstLetter()
     }
 }
+
+// MAKR: - Validation
+extension String {
+    func isValidNickname() -> Bool {
+        let nicknameRegEx = "[a-zA-Z0-9가-힣]{2,9}"
+        let predicate = NSPredicate(format: "SELF MATCHES %@", nicknameRegEx)
+        return predicate.evaluate(with: self)
+    }
+    
+    func containsKoreanVowelsAndConsonants() -> Bool {
+        let koreanRegEx = "[ㄱ-ㅎㅏ-ㅣ]"
+        return self.range(of: koreanRegEx, options: .regularExpression) != nil
+    }
+}
